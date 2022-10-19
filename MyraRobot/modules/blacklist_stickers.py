6203ddc62df9e40245e141dd38d0be50b88084b3
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-19 21:27:20 +0530

    Update blacklist_stickers.py

--- MyraRobot/modules/blacklist_stickers.py
+++ MyraRobot/modules/blacklist_stickers.py
@@ -1,16 +1,16 @@
 import html
 from typing import Optional
 
-import FallenRobot.modules.sql.blsticker_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.warns import warn
+import MyraRobot.modules.sql.blsticker_sql as sql
+from MyraRobot import LOGGER, dispatcher
+from MyraRobot.modules.connection import connected
+from MyraRobot.modules.disable import DisableAbleCommandHandler
+from MyraRobot.modules.helper_funcs.alternate import send_message
+from MyraRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from MyraRobot.modules.helper_funcs.misc import split_message
+from MyraRobot.modules.helper_funcs.string_handling import extract_time
+from MyraRobot.modules.log_channel import loggable
+from MyraRobot.modules.warns import warn
 from telegram import Chat, ChatPermissions, Message, ParseMode, Update, User
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
