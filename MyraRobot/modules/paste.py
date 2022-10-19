5bc99dd1760777987335ecc1b7ee103ed195f03d
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-19 22:32:14 +0530

    Update paste.py

--- MyraRobot/modules/paste.py
+++ MyraRobot/modules/paste.py
@@ -3,10 +3,10 @@ import os
 import re
 
 import aiofiles
-from FallenRobot import aiohttpsession as session
-from FallenRobot import pbot as app
-from FallenRobot.utils.errors import capture_err
-from FallenRobot.utils.pastebin import paste
+from MyraRobot import aiohttpsession as session
+from MyraRobot import pbot as app
+from MyraRobot.utils.errors import capture_err
+from MyraRobot.utils.pastebin import paste
 from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
