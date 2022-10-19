8f3f267e2148a4fc382cf17287680167be78763a
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-20 00:08:37 +0530

    Update chat_status.py

--- MyraRobot/modules/helper_funcs/chat_status.py
+++ MyraRobot/modules/helper_funcs/chat_status.py
@@ -401,6 +401,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from FallenRobot.modules import connection
+from MyraRobot.modules import connection
 
 connected = connection.connected
