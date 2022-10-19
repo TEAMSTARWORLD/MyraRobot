d10204029c237f756064f1a856c11f774ef1a14d
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-19 15:24:46 +0530

    Update alive.py

--- MyraRobot/modules/alive.py
+++ MyraRobot/modules/alive.py
@@ -1,8 +1,8 @@
 import random
 
-from FallenRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
-from FallenRobot import telethn as tbot
-from FallenRobot.events import register
+from MyraRobot import OWNER_USERNAME, SUPPORT_CHAT, dispatcher
+from MyraRobot import telethn as tbot
+from MyraRobot.events import register
 from pyrogram import __version__ as pyrover
 from telegram import __version__ as telever
 from telethon import Button
@@ -10,14 +10,14 @@ from telethon import __version__ as tlhver
 
 PHOTO = [
     "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
-    "https://telegra.ph/file/40eb1ed850cdea274693e.jpg",
+    "https://telegra.ph/file/06df9b4cc15a1ed7b0eec.jpg",
 ]
 
 
 @register(pattern=("/alive"))
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗦𝗧𝝙𝗥𝗪𝗢𝗥𝗟𝗗](https://t.me/{OWNER_USERNAME})** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
