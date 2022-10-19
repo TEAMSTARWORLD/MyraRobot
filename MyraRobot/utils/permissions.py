f5be93fead788a55e87dc7a087dc254460a89a54
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-19 12:24:55 +0530

    Update permissions.py

--- MyraRobot/utils/permissions.py
+++ MyraRobot/utils/permissions.py
@@ -1,8 +1,8 @@
 from functools import wraps
 
-from FallenRobot import DEV_USERS
-from FallenRobot import pbot as app
-from FallenRobot.utils.adminperms import member_permissions
+from MyraRobot import DEV_USERS
+from MyraRobot import pbot as app
+from MyraRobot.utils.adminperms import member_permissions
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
 
