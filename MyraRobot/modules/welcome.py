37c8bd961cbbf0bca93ae7fe532632a824f448a9
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-19 12:37:46 +0530

    Update welcome.py

--- MyraRobot/modules/welcome.py
+++ MyraRobot/modules/welcome.py
@@ -5,9 +5,9 @@ import time
 from contextlib import suppress
 from functools import partial
 
-import FallenRobot
-import FallenRobot.modules.sql.welcome_sql as sql
-from FallenRobot import (
+import MyraRobot
+import MyraRobot.modules.sql.welcome_sql as sql
+from MyraRobot import (
     DEMONS,
     DEV_USERS,
     DRAGONS,
@@ -18,18 +18,18 @@ from FallenRobot import (
     WOLVES,
     dispatcher,
 )
-from FallenRobot.modules.helper_funcs.chat_status import (
+from MyraRobot.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from FallenRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from FallenRobot.modules.helper_funcs.msg_types import get_welcome_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+from MyraRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from MyraRobot.modules.helper_funcs.msg_types import get_welcome_type
+from MyraRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.global_bans_sql import is_user_gbanned
+from MyraRobot.modules.log_channel import loggable
+from MyraRobot.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
