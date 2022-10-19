635ba8c2fb94955b238e6da1f8d801aa1428834b
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-10-19 21:41:35 +0530

    Update cust_filters.py

--- MyraRobot/modules/cust_filters.py
+++ MyraRobot/modules/cust_filters.py
@@ -3,17 +3,17 @@ import re
 from html import escape
 
 import telegram
-from FallenRobot import DRAGONS, LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
-from FallenRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from FallenRobot.modules.helper_funcs.misc import build_keyboard_parser
-from FallenRobot.modules.helper_funcs.msg_types import get_filter_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+from MyraRobot import DRAGONS, LOGGER, dispatcher
+from MyraRobot.modules.connection import connected
+from MyraRobot.modules.disable import DisableAbleCommandHandler
+from MyraRobot.modules.helper_funcs.alternate import send_message, typing_action
+from MyraRobot.modules.helper_funcs.chat_status import user_admin
+from MyraRobot.modules.helper_funcs.extraction import extract_text
+from MyraRobot.modules.helper_funcs.filters import CustomFilters
+from MyraRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from MyraRobot.modules.helper_funcs.misc import build_keyboard_parser
+from MyraRobot.modules.helper_funcs.msg_types import get_filter_type
+from MyraRobot.modules.helper_funcs.string_handling import (
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
